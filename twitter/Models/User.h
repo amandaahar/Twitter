26a6ff0fce3416b12ac3f0410d5fca13a4288c92
Amanda Harris <amandahar@fb.com>
2019-07-01 17:38:35 -0700

    added data models for user and tweet

---
+++ twitter/Models/User.h
@@ -0,0 +1,26 @@
+//
+//  User.h
+//  twitter
+//
+//  Created by amandahar on 7/1/19.
+//  Copyright © 2019 Emerson Malca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface User : NSObject
+
+@property (strong, nonatomic) NSString *name;
+@property (strong, nonatomic) NSString *screenName;
+
+// create initializer to set all properties of a user based off the dictionary returned
+// initializerd are kind of like contructors
+// instance type refers to the type of the file so this initializer will return a user
+-(instancetype)initWithDictionary:(NSDictionary *) dictionary;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ twitter/Models/User.m
@@ -0,0 +1,25 @@
+//
+//  User.m
+//  twitter
+//
+//  Created by amandahar on 7/1/19.
+//  Copyright © 2019 Emerson Malca. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+-(instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    // [super init] initializes an object with the parent's class initializer
+    self = [super init];
+    
+    if(self) {
+        self.name = dictionary[@"name"];
+        self.screenName = dictionary[@"screen_name"];
+    }
+    return self;
+    
+}
+
+@end
